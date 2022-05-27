f2bb537b719c805d427d9463841c8e8c1282a51c
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-05-28 02:59:58 +0300

    hebele

--- Assets/Kayribuba/Scripts 1/CameraScroll.cs
+++ Assets/Kayribuba/Scripts 1/CameraScroll.cs
@@ -6,15 +6,11 @@ public class CameraScroll : MonoBehaviour
 {
     public float acc = 1;
     public float speedModifier = .5f;
-    public float speed = 0;
-
-    float time = 0;
+    public float speed = 5;
 
     void Update()
     {
-        time = Time.time;
-
-        speed = acc * time;
+        speed += acc * Time.deltaTime;
 
         transform.Translate(speed * speedModifier, 0, 0);
     }
