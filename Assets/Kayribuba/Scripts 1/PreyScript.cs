c553ab8ec5ca98ea9c5e7e771fba25c86d7ddc60
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-05-28 08:47:10 +0300

    sa

--- Assets/Kayribuba/Scripts 1/PreyScript.cs
+++ Assets/Kayribuba/Scripts 1/PreyScript.cs
@@ -1,16 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+    enum PreyState { Idle, Walk, Flee}
 
 public class PreyScript : MonoBehaviour
 {
+    [SerializeField] Transform edgeCheck;
+    [SerializeField] LayerMask GroundLayers;
+    [SerializeField] float speed = 3f;
+    [SerializeField] float edgeCheckRadius = 1f;
+
+    PreyState currentState = PreyState.Idle;
+    float changeBehaviorAt = float.MaxValue;
+    bool changeAgain = true;
+    Rigidbody2D rb;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        
+        if (changeAgain)
+        {
+            changeBehaviorAt = Random.Range(1, 5) + Time.time;
+            changeAgain = false;
+        }
+
+        if(changeBehaviorAt <= Time.time)
+        {
+            if(currentState == PreyState.Idle)
+            {
+                currentState = PreyState.Walk;
+            }
+            else if (currentState == PreyState.Walk)
+            {
+                currentState = PreyState.Idle;
+            }
+            changeAgain = true;
+        }
+
+        DoStates();
     }
 
     public void Die()
@@ -18,8 +49,28 @@ public class PreyScript : MonoBehaviour
         //GE��C�
         transform.Find("Sprite").GetComponent<SpriteRenderer>().color = Color.green;
         GetComponent<CapsuleCollider2D>().enabled = false;
-        Destroy(GetComponent<Rigidbody2D>());
+        Destroy(rb);
         this.enabled = false;
         //GE��C�
+    }
+    void DoStates()
+    {
+        if(currentState == PreyState.Idle)
+        {
+
+        }
+        else if (currentState == PreyState.Walk)
+        {
+            rb.velocity = speed * transform.forward;
+            if (Physics2D.OverlapCircle(edgeCheck.position, edgeCheckRadius, GroundLayers))
+            {
+
+            }
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(edgeCheck.position, edgeCheckRadius);
     }
 }
