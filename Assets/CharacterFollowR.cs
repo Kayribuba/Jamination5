3eeddb254eb815b14cab314621520f397d8511ea
asliceng <asli.cengiz@bahcesehir.edu.tr>
2022-05-29 08:27:43 +0300

    zıplayıverçekirge

--- Assets/CharacterFollowR.cs
+++ Assets/CharacterFollowR.cs
@@ -4,24 +4,12 @@ public class CharacterFollowR : MonoBehaviour
 {
     [Range(0,10)]public float change = 0.5f;
     public CameraScroll cs;
-    
 
-
-    void Start()
-    {
-        
-    }
     public void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-
+    {       
         if (collision.gameObject.CompareTag("Player"))
         {
-            
-
             cs.speed += change;
-            Debug.Log(cs.speed);
-
         }
     }
 
