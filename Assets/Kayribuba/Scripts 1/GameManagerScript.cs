46a84282a4cfa5bb98b8d28c999c664e1a8d2d48
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-05-29 13:08:38 +0300

    push adasrapekşe

--- Assets/Kayribuba/Scripts 1/GameManagerScript.cs
+++ Assets/Kayribuba/Scripts 1/GameManagerScript.cs
@@ -16,18 +16,32 @@ public class GameManagerScript : MonoBehaviour
     public GameObject[] Courses;
 
     GameObject lastCourse, currentCourse, nextCourse;
+    Camera cam;
 
     void Start()
     {
-
+        cam = Camera.main;
+        Vector3 spawnPos = cam.transform.position;
+        spawnPos.z = 0;
+        currentCourse = Instantiate(Courses[0], spawnPos, Quaternion.identity);
+        lastCourse = Instantiate(Courses[0], spawnPos, Quaternion.identity);
     }
     void Update()
     {
         EditorMode();
+
+        if (Camera.main.transform.position.x >= currentCourse.transform.position.x)
+            SpawnNextCourses();
     }
 
     void SpawnNextCourses()
     {
+        Destroy(lastCourse.gameObject);
+        lastCourse = currentCourse;
+
+        Vector3 spawnPoint = cam.transform.position;
+        spawnPoint.x += 70;
+        currentCourse = Instantiate(Courses[0], spawnPoint, Quaternion.identity);
 
     }
     void EditorMode()
